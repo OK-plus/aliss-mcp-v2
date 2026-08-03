888fba9ce7f9e74e6de7c78431a713ee00f97d29
OK-plus <charlie@okpositive.co.uk>
2026-08-03 11:11:58 +0100

    Add stateless_http option to mcp.run_async

--- server.py
+++ server.py
@@ -29,10 +29,11 @@ if __name__ == "__main__":
         f"Starting ALISS MCP on port {os.getenv('PORT', 8080)}"
     )
 
-    asyncio.run(
-        mcp.run_async(
-            transport="streamable-http",
-            host="0.0.0.0",
-            port=int(os.getenv("PORT", "8080")),
-        )
+   asyncio.run(
+    mcp.run_async(
+        transport="streamable-http",
+        host="0.0.0.0",
+        port=int(os.getenv("PORT", "8080")),
+        stateless_http=True,
     )
+)
